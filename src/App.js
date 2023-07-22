da5ecbcfe09be0cfd30da05bf1dd698e30b3bfae
franagan <118491675+franagan@users.noreply.github.com>
2023-07-22 20:03:42 +0200

    cmabios pull

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./componentes/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import Comercios from "./pages/Comercios/Comercios";
-import Comercio1 from "./pages/Comercios/Comercio1";
-import Comercio2 from "./pages/Comercios/Comercios";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import About from "./pages/About/About";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './componentes/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Comercios from './pages/Comercios/Comercios';
+import Comercio1 from './pages/Comercios/Comercio1';
+import Comercio2 from './pages/Comercios/Comercios';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import About from './pages/About/About';
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/comercios" element={<Comercios />} />
-          <Route path="/comercio1" element={<Comercio1 />} />
-          <Route path="/comercio2" element={<Comercio2 />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/comercios" element={<Comercios />} />
+                    <Route path="/comercio1" element={<Comercio1 />} />
+                    <Route path="/comercio2" element={<Comercio2 />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
