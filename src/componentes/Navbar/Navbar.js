bcb8932124216d27470c23ccb22216df7cb4794f
Iván Villena González <ivan.villenag@gmail.com>
2023-07-23 17:07:36 +0200

    Actualización LazyLoad y fix de Navbar

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -1,51 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./componentes/Navbar/Navbar";
 
-export const Navbar = () => {
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Comercios = lazy(() => import("./pages/Comercios/Comercios"));
+const Comercio1 = lazy(() => import("./pages/Comercios/Comercio1"));
+const Comercio2 = lazy(() => import("./pages/Comercios/Comercio2"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Register = lazy(() => import("./pages/Register/Register"));
+const About = lazy(() => import("./pages/About/About"));
+
+const App = () => {
   return (
-    <div className="navbar-container">
-      <ul className="nav nav-tabs navbar-custom">
-        <li className="nav-item">
-          <Link className="nav-link" to="/">
-            Incio</Link>
-        </li>
-        <li className="nav-item dropdown">
-          <Link
-            className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Comercios</Link>
-          <ul className="dropdown-menu">
-            <li>
-              <Link className="dropdown-item" to="comercios">Comercios</Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="comercio1">Comercio 1</Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="comercio2">Comercio 2</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Login</Link>
-          <ul className="dropdown-menu">
-            <li>
-              <Link className="dropdown-item" to="login">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="register">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="about">About Us</Link>
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Suspense fallback={<div>Cargando página...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/comercios" element={<Comercios />} />
+            <Route path="/comercio1" element={<Comercio1 />} />
+            <Route path="/comercio2" element={<Comercio2 />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 };
 
-export default Navbar;
+export default App;
