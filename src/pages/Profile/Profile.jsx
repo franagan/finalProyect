fec3e7a67a9a304ea80af4d16125fe9376912cd3
Shurjish <Cperezbarroso@gmail.com>
2023-07-26 19:50:22 +0200

    se añade eliminar usuario

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, NavLink } from 'react-router-dom';
+import { useAuth } from '../../Context/AuthProvider';
 
 const Profile = () => {
   const { userId } = useParams();
   const [user, setUser] = useState(null);
+  const { logout } = useAuth();
 
   useEffect(() => {
     const getUserData = async () => {
@@ -23,6 +25,27 @@ const Profile = () => {
     return <div>Cargando...</div>;
   }
 
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`https://backfinalproyect.vercel.app/user/${id}`);
+      sessionStorage.removeItem('token');
+
+    } catch (error) {
+      console.error('Error al eliminar el usuario:', error);
+    }
+  };
+
+  const handleDeleteClick = () => {
+    const confirmed = window.confirm('¿Estás seguro de que deseas eliminar tu cuenta?');
+    if (confirmed) {
+      deleteUser(userId).then(() => {
+        logout();
+      }).catch((error) => {
+        console.error('Error al eliminar el usuario:', error);
+      });
+    }
+  };
+
   return (
     <div className="profile-card">
       <h3>Hola {user.name}, estos son tus datos:</h3>
@@ -31,8 +54,8 @@ const Profile = () => {
       <p>Apellido: {user.lastname}</p>
       <p>Teléfono: {user.phone}</p>
 
-
       <NavLink to={`/editProfile/${userId}`} className="btn btn-primary">Editar Perfil</NavLink>
+      <button className="btn btn-primary" onClick={handleDeleteClick}>Eliminar Usuario</button>
     </div>
   );
 };
